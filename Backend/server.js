0059f2982e7d43cba2bb956d00b6751f643a7258
Shubhankit Jain <shubhankitjain@Shubhankits-MacBook-Air.local>
2024-01-04 16:19:06 +0530

    Added DOTENV Suport in the DB 🦈

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const router = require("./Controllers/HomeRoute");
 const app = express();
-const mongoose = require("mongoose");
 const connectDB = require("./Config/ConnectDB");
 
 connectDB();
