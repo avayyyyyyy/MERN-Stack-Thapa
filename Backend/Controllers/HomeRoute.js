6119be2ae228be8e9289256c6f7d93fb806d1e7d
avayyyyyyy <jabhi465@gmail.com>
2024-01-04 16:41:53 +0530

    Added a registeration route 🧡

--- Backend/Controllers/HomeRoute.js
+++ Backend/Controllers/HomeRoute.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const User = require("../Models/user-model");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hello From Home");
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, phone, password } = req.body;
+    let inserted = await new User({
+      username,
+      email,
+      phone,
+      password,
+    });
+    await inserted.save();
+    res.json({ msg: "Data inserted successfully" });
+  } catch (e) {
+    res.json({ err: "Insert Correct and Unique Data " });
+  }
 });
 
 module.exports = router;
--- Backend/Models/user-model.js
+++ Backend/Models/user-model.js
@@ -8,6 +8,7 @@ let userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   phone: {
     type: String,
